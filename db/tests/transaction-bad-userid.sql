39c9c4cf6459d83d281c2d798f1c9a8e0b54997f
Steven Taschuk <steven@amotlpaa.org>
2014-11-19 15:13:33 -0500

    Test that transactions need a valid yd_userid.

---
+++ db/tests/transaction-bad-userid.sql
@@ -0,0 +1,29 @@
+-- Tests that inserting a transaction with an invalid user ID fails.
+-- Normally this would be a silly test because there would be a
+-- foreign key constraint on the transaction table, and what's the
+-- point of us testing PostgreSQL's foreign key implementation?
+-- But this particular constraint is enforced by a trigger whose
+-- code we wrote ourselves, so it's testworthy.  (See create.sql for
+-- explanation of why it's not a constraint.)
+\i create.sql
+\unset ON_ERROR_STOP
+begin;
+select max(id)+1 as id from yd_userV
+    \gset bad
+insert into transaction (yd_userid, yd_ipaddr, yd_useragent)
+values (:badid, '127.0.0.1', :'script');
+end;
+
+begin;
+insert into transaction (yd_userid, yd_ipaddr, yd_useragent)
+values (0, '127.0.0.1', :'script')
+returning id
+    \gset txn
+insert into yd_userV (id, txnid, obj)
+values (0, :txnid, null);  -- delete user 0; don't do this for real!
+end;
+
+begin;
+insert into transaction (yd_userid, yd_ipaddr, yd_useragent)
+values (0, '127.0.0.1', :'script');
+end;
