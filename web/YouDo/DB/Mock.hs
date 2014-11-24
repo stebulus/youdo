09e8bb0a99538c0482d37d23add5e49c1fecb6cf
Steven Taschuk <steven@amotlpaa.org>
2014-11-24 11:45:16 -0500

    Add YouDo.DB.Mock with trivial in-memory implementation.

---
+++ web/YouDo/DB/Mock.hs
@@ -0,0 +1,20 @@
+module YouDo.DB.Mock where
+
+import Prelude hiding (id)
+import Control.Concurrent.MVar (MVar, withMVar, modifyMVar)
+import Data.Maybe (fromJust, listToMaybe)
+import YouDo.DB (Youdo(..), DB(..))
+
+data BareMockDB = BareMockDB { youdos :: [Youdo] }
+data MockDB = MockDB { mvar :: MVar BareMockDB }
+
+instance DB MockDB where
+    getYoudo ydid db = withMVar (mvar db) $ \db ->
+        return $ [yd | yd<-youdos db, maybe False (==ydid) (id yd)]
+    getYoudos db = withMVar (mvar db) $ \db ->
+        return $ youdos db
+    postYoudo yd db = modifyMVar (mvar db) $ \db -> do
+        let newid = 1 + fromJust (maybe (Just 0) id $ listToMaybe $ youdos db)
+        return ( db { youdos = yd { id = Just newid } : (youdos db) }
+               , newid
+               )
