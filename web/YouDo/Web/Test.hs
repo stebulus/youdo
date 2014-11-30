bab117d5fa9851a826349f746e7ad6dd21f22724
Steven Taschuk <steven@amotlpaa.org>
2014-11-30 09:21:49 -0500

    Add EDSL for constructing Request objects.

--- web/YouDo/Web/Test.hs
+++ web/YouDo/Web/Test.hs
@@ -9,14 +9,15 @@ import Data.ByteString.Lazy (ByteString)
 import qualified Data.ByteString.Char8 as SB
 import Data.CaseInsensitive (CI, mk)
 import Data.Maybe (fromJust)
-import Data.Monoid (mempty, (<>))
+import Data.Monoid ((<>), Monoid(..))
 import Distribution.TestSuite (Test(..), TestInstance(..), Progress(..),
     Result(..))
 import Network.HTTP.Types (Status, ResponseHeaders, ok200, created201, http11,
     methodGet, methodPost, decodePathSegments, parseQuery, Method)
 import Network.Socket (SockAddr(..))
 import Network.URI (parseURI, URI(..), URIAuth(..))
-import Network.Wai (Application, responseToStream, RequestBodyLength(..), requestBody)
+import Network.Wai (Application, responseToStream, RequestBodyLength(..), requestBody,
+    defaultRequest)
 import Network.Wai.Internal (Request(..), ResponseReceived(..))
 import Web.Scotty (scottyApp)
 import YouDo.Web (app, withDB, DBOption(..))
@@ -25,20 +26,21 @@ tests :: IO [Test]
 tests = return
     [ sillyTest
     , serverTest "new youdo" $ \req -> do
-        thereq <- liftIO
-            $ setBody "assignerid=0&assigneeid=0&description=blah&duedate=&completed=false"
-            $ addHeader "Content-Type" "application/x-www-form-urlencoded"
-            $ basicRequest methodPost "http://example.com/0/youdos"
-        (stat, _, _) <- liftIO $ req thereq
+        (stat, _, _) <- liftIO $ req
+            $ method methodPost
+            <> url "http://example.com/0/youdos"
+            <> body "assignerid=0&assigneeid=0&description=blah&duedate=&completed=false"
+            <> header "Content-Type" "application/x-www-form-urlencoded"
         stat ~= created201
     ]
 
 sillyTest :: Test
 sillyTest = serverTest "silly" $ \req -> do
-    (stat, _, body) <- liftIO $ req $
-        basicRequest methodGet "http://example.com/"
+    (stat, _, bod) <- liftIO $ req
+        $ method methodGet
+        <> url "http://example.com/"
     stat ~= ok200
-    body ~= "placeholder"
+    bod ~= "placeholder"
 
 (~=) :: (Eq a, Show a, Monad m) => a -> a -> EitherT String m ()
 x ~= y = if x == y
@@ -46,15 +48,16 @@ x ~= y = if x == y
          else left $ "expected " ++ (show y) ++ ", got " ++ (show x)
 infix 1 ~=
 
-serverTest :: String
-    -> ((Request -> IO (Status, ResponseHeaders, ByteString))
+serverTest :: (IsRequest a) =>
+    String
+    -> ((a -> IO (Status, ResponseHeaders, ByteString))
         -> EitherT String IO ())
     -> Test
 serverTest testName f = Test $ TestInstance
     { run = withDB InMemory $ \db -> do
         mvdb <- newMVar db
         waiApp <- scottyApp $ app (fromJust $ parseURI "http://example.com") mvdb
-        result <- runEitherT $ f $ request waiApp . return
+        result <- runEitherT $ f $ request waiApp
         return $ Finished $ case result of
             Left msg -> Fail msg
             Right _ -> Pass
@@ -65,8 +68,8 @@ serverTest testName f = Test $ TestInstance
     }
 
 basicRequest :: Method -> String -> Request
-basicRequest method uri = Request
-    { requestMethod = method
+basicRequest meth uri = Request
+    { requestMethod = meth
     , httpVersion = http11
     , pathInfo = decodePathSegments path
     , rawPathInfo = path
@@ -77,44 +80,75 @@ basicRequest method uri = Request
     , remoteHost = SockAddrInet 0 0
     , requestBody = return ""
     , vault = mempty
-    , requestBodyLength = KnownLength $ fromIntegral $ SB.length body
+    , requestBodyLength = KnownLength $ fromIntegral $ SB.length bod
     , requestHeaderHost = Just host
     , requestHeaderRange = Nothing
     }
-    where body = ""::SB.ByteString
+    where bod = ""::SB.ByteString
           host = SB.pack $ uriRegName $ fromJust $ uriAuthority parseduri
           query = SB.pack $ uriQuery parseduri
           path = SB.pack $ uriPath parseduri
           parseduri = fromJust $ parseURI uri
 
-setBody :: String -> Request -> IO Request
-setBody body req = do
+class IsRequest a where
+    toRequest :: a -> IO Request
+instance IsRequest Request where
+    toRequest = return
+data RequestTransformer = RequestTransformer (IO Request -> IO Request)
+instance IsRequest RequestTransformer where
+    toRequest (RequestTransformer xfm) = xfm $ return defaultRequest
+instance Monoid RequestTransformer where
+    mempty = RequestTransformer id
+    mappend (RequestTransformer f) (RequestTransformer g)
+        = RequestTransformer (f.g)
+
+method :: Method -> RequestTransformer
+method meth = RequestTransformer $ \ioreq -> do
+    req <- ioreq
+    return req { requestMethod = meth }
+
+url :: String -> RequestTransformer
+url u = RequestTransformer $ \ioreq -> do
+    req <- ioreq
+    return req { pathInfo = decodePathSegments path
+               , rawPathInfo = path
+               , rawQueryString = query
+               , queryString = parseQuery query
+               }
+        where query = SB.pack $ uriQuery parseduri
+              path = SB.pack $ uriPath parseduri
+              parseduri = fromJust $ parseURI u
+
+body :: SB.ByteString -> RequestTransformer
+body s = RequestTransformer $ \ioreq -> do
+    req <- ioreq
     -- The IO action in requestBody in Request is supposed to produce
     -- successive chunks, and "" when there aren't any more.  Thus:
-    iobody <- successively $ (SB.pack body) : repeat ""
-    return req
-        { requestBody = iobody
-        , requestBodyLength = KnownLength $ fromIntegral $ length body
-        }
+    iobody <- successively $ s : repeat ""
+    return req { requestBody = iobody
+               , requestBodyLength = KnownLength $ fromIntegral $ SB.length s
+               }
 
-addHeader :: SB.ByteString -> SB.ByteString -> Request -> Request
-addHeader key val req =
-    req { requestHeaders = (mk key, val) : requestHeaders req }
+header :: SB.ByteString -> SB.ByteString -> RequestTransformer
+header key val = RequestTransformer $ \ioreq -> do
+    req <- ioreq
+    return req { requestHeaders = (mk key, val) : requestHeaders req }
 
-request :: Application -> IO Request -> IO (Status, ResponseHeaders, ByteString)
-request appl ioreq = do
+request :: (IsRequest a) =>
+    Application -> a -> IO (Status, ResponseHeaders, ByteString)
+request appl req = do
     mvresp <- newEmptyMVar
     mvbody <- newMVar ""
-    req <- ioreq
-    _ <- appl req $ \resp -> do
+    req' <- toRequest req
+    _ <- appl req' $ \resp -> do
         let (status, hdrs, stream) = responseToStream resp
         stream (\streamingbody ->
             let write builder = modifyMVar_ mvbody
                     (\x -> return (x <> toLazyByteString builder))
                 flush = return ()
             in streamingbody write flush)
-        body <- takeMVar mvbody
-        putMVar mvresp (status, hdrs, body)
+        bod <- takeMVar mvbody
+        putMVar mvresp (status, hdrs, bod)
         return ResponseReceived
     takeMVar mvresp
 
