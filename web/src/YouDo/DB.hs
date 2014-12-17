465ba3f452ac3aea01cdeea459279d493432bbb2
Steven Taschuk <steven@amotlpaa.org>
2014-12-17 17:51:54 -0500

    Delete superfluous (Show k) context on webdb.

--- web/src/YouDo/DB.hs
+++ web/src/YouDo/DB.hs
@@ -110,16 +110,16 @@ class Updater u a where
 
     These correspond directly to the methods of 'DB'.  The name @objs@
     is obtained from the instance 'NamedResource' @k@.  Requests that
-    return objects return them in JSON format, using the instances
-    'Show' @k@ and 'BasedToJSON' @v@.  The @/id/@ parameter is interpreted
+    return objects return them in JSON format, using the instance
+    'BasedToJSON' @v@.  The @/id/@ parameter is interpreted
     via the instance 'Parsable' @k@.  (The 'FromJSON' @k@ instance
     would only be used if the id were passed in the JSON request
     body, which it shouldn't be.)  The request body, when needed,
-    is interpreted via default 'RequestParser' for the appropriate type.
+    is interpreted via 'body'.
 -}
 webdb :: ( NamedResource k, DB k v u IO d
          , Parsable k, FromJSON k
-         , Show k, BasedToJSON v
+         , BasedToJSON v
          , RequestParsable v
          , RequestParsable u
          ) => MVar ()     -- ^All database access is under this MVar.
