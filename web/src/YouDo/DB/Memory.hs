40a2d7edb6b819b7b745776af730137b99e1795c
Steven Taschuk <steven@amotlpaa.org>
2014-12-18 16:11:26 -0500

    Use IORef, not MVar, in DB.Memory.  (Use LockDB for thread-safety.)

--- web/src/YouDo/DB/Memory.hs
+++ web/src/YouDo/DB/Memory.hs
@@ -1,7 +1,8 @@
 {-# LANGUAGE MultiParamTypeClasses, FlexibleContexts, FlexibleInstances #-}
-module YouDo.DB.Memory where
+module YouDo.DB.Memory (MemoryDB, empty) where
 import Control.Applicative ((<$>))
-import Control.Concurrent.MVar (MVar, withMVar, modifyMVar, newMVar)
+import Control.Concurrent.MVar (newMVar)
+import Data.IORef (IORef, newIORef, readIORef, writeIORef, modifyIORef')
 import Data.Function (on)
 import Data.List (nubBy)
 import Data.Maybe (listToMaybe)
@@ -9,50 +10,66 @@ import Data.Maybe (listToMaybe)
 import YouDo.DB
 import YouDo.Types
 
+-- | A transient in-memory implementation of 'DB'.
+-- Note that this object is not thread-safe; wrap it in a 'LockDB' if needed.
 data (Updater u v) => MemoryDB k v u
-    = MemoryDB { mvar :: MVar [Versioned k v]
+    = MemoryDB { things :: IORef [Versioned k v]
                , nextID :: IO k
                , nextTxnID :: IO TransactionID
                }
 
 newdb :: (Updater u v) => (Int->k) -> IO TransactionID -> IO (MemoryDB k v u)
 newdb toID nextTxn = do
-    mv <- newMVar []
+    ioref <- newIORef []
     incr <- (fmap.fmap) toID $ newIncrementer 1
-    return $ MemoryDB mv incr nextTxn
+    return $ MemoryDB ioref incr nextTxn
 
+-- Not thread-safe!
 newIncrementer :: (Enum a) => a -> IO (IO a)
 newIncrementer initial = do
-    mv <- newMVar initial
-    return $ modifyMVar mv $ \n -> return (succ n, n)
+    ioref <- newIORef initial
+    return $ do
+        n <- readIORef ioref
+        let m = succ n
+        writeIORef ioref m
+        return n
 
 instance (Eq k, NamedResource k, Updater u v)
          => DB IO k v u (MemoryDB k v u) where
-    get k d = withMVar (mvar d) $ \xs ->
+    get k d = do
+        xs <- readIORef $ things d
         return $ one $ [x | x<-xs, thingid (version x) == k]
-    getVersions k d = withMVar (mvar d) $ \xs ->
+    getVersions k d = do
+        xs <- readIORef $ things d
         return $ some $ [x | x<-xs, thingid (version x) == k]
-    getVersion vk d = withMVar (mvar d) $ \xs ->
+    getVersion vk d = do
+        xs <- readIORef $ things d
         return $ one $ [x | x<-xs, version x == vk]
-    getAll d = withMVar (mvar d) $ \xs ->
+    getAll d = do
+        xs <- readIORef $ things d
         return $ success $ nubBy ((==) `on` thingid . version) xs
-    create v d = modifyMVar (mvar d) $ \xs -> do
+    create v d = do
         newid <- nextID d
         newtxn <- nextTxnID d
         let newx = Versioned (VersionedID newid newtxn) v
-        return (newx : xs, success newx)
-    update vu d = modifyMVar (mvar d) $ \xs -> do
+        modifyIORef' (things d) (newx : )
+        return $ success newx
+    update vu d = do
+        xs <- readIORef $ things d
         let oldx = listToMaybe [ x | x<-xs, thingid (version x) == thingid (version vu) ]
         case oldx of
-            Nothing -> return (xs, notFound)
+            Nothing -> return notFound
             Just x -> if version x /= version vu
-                        then return (xs, newerVersion x)
+                        then return $ newerVersion x
                         else do newtxn <- nextTxnID d
                                 let newx = Versioned
                                             (VersionedID (thingid (version x)) newtxn)
                                             (doUpdate (thing vu) (thing x))
-                                return (newx:xs, success newx)
+                                modifyIORef' (things d) (newx : )
+                                return $ success newx
 
+-- | Create a new empty 'YoudoDatabase' backed by 'MemoryDB's.
+-- All operations (to either 'DB') are locked under a single MVar.
 empty :: IO (YoudoDatabase
                 (LockDB IO YoudoID YoudoData YoudoUpdate
                     (MemoryDB YoudoID YoudoData YoudoUpdate))
